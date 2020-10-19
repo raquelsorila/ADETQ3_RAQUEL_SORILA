7cb354a30eb278db2509c11f1a627783b8cb7c91
Lenovo Ideapad A320 <Lenovo Ideapad A320@DESKTOP-10K0GFT>
2020-10-19 20:26:18 +0800

    ADETQ3_RAQUEL_SORILA

---
+++ ADETQ3_RAQUEL_SORILA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_RAQUEL_SORILA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
